e16dfacf941c5c2fad94088e0a8b6670e24f0456
sdjeny <s@windows10.microdone.cn>
2020-07-04 13:47:01 +0800

    message

--- wxapp/src/main/java/wxapp/MainTest.java
+++ wxapp/src/main/java/wxapp/MainTest.java
@@ -1,17 +1,26 @@
 package wxapp;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import wxapp.util.HttpClientHelper;
+import wxapp.util.JsonFactory;
 
 @Controller
 @EnableAutoConfiguration
@@ -22,25 +31,67 @@ public class MainTest {
 		SpringApplication.run(MainTest.class, args);
 	}
 
-	@RequestMapping("/getinfo/{appid}/{code}/{time}/{verify}")
+//	@Bean
+//	public ServletWebServerFactory servletContainer() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//		return tomcat;
+//	}
+//
+//	private Connector createStandardConnector() {
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setPort(8122);
+//		return connector;
+//	}
+
+	@RequestMapping("/subscribe_send/{appid}/{code}/{tempid}/{verify}")
 	@ResponseBody
-	private String getInfo(//
+	private String subscribe_send(//
 			@PathVariable("appid") String appid//
 			, @PathVariable("code") String code//
-			, @PathVariable("time") long time//
+			, @PathVariable("tempid") String tempid//
 			, @PathVariable("verify") long verify//
 	) {
-		Map<String, Object> result = new HashMap<>();
-		Map<String, Object> data = new HashMap<>();
-		data.put("appid", appid);
-		data.put("code", code);
-		data.put("time", time);
-		data.put("verify", verify);
-		result.put("data", data);
-		result.put("err", "");
 		try {
+			Map<String, Object> params = new LinkedHashMap<>();
+			Map<String, Object> rep;
+			params.put("appid", appid);
+			params.put("secret", "914aecbc1cb96e500ca094376f48b844");
+			params.put("grant_type", "client_credential");
+			rep = JsonFactory.fromJson(
+					HttpClientHelper.getInstance().doGet("https://api.weixin.qq.com/cgi-bin/token", params), Map.class);
+			String access_token = (String) rep.get("access_token");
+			params.put("js_code", code);
+			params.put("grant_type", "authorization_code");
+			rep = (Map<String, Object>) JsonFactory.fromJson(
+					HttpClientHelper.getInstance().doGet("https://api.weixin.qq.com/sns/jscode2session", params),
+					Object.class);
+			String openid = (String) rep.get("openid");
+			params.clear();
+			Function<String, Map<String, String>> valueMap = s -> {
+				Map<String, String> result = new HashMap<>();
+				result.put("value", s);
+				return result;
+			};
+			Map<String, Object> data = new LinkedHashMap<>();
+			data.put("thing1", valueMap.apply("Title"));
+			data.put("date3", valueMap.apply("2019年10月1日"));
+			data.put("phone_number6", valueMap.apply("18888888888"));
+			data.put("amount5", valueMap.apply("100"));
+			data.put("character_string7", valueMap.apply("W443544333f3"));
+//			params.put("access_token", access_token);
+//			params.put("appid", appid);
+			params.put("touser", openid);
+			params.put("template_id", tempid);
+			params.put("data", data);
+			rep = (Map<String, Object>) JsonFactory.fromJson(HttpClientHelper.getInstance().doPost(
+					"https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=" + access_token, params,
+					true), Object.class);
+			Map<String, Object> result = new HashMap<>();
+			result.put("req", rep);
+			result.put("err", "");
 			return wxapp.util.JsonFactory.toJson(result);
-		} catch (JsonProcessingException e) {
+		} catch (Throwable e) {
 			return String.format("{\"err\":\"%s\"}", e.getMessage());
 		}
 	}
---
+++ wxapp/src/main/java/wxapp/util/HttpClientHelper.java
@@ -0,0 +1,238 @@
+package wxapp.util;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientHelper {
+	private static PoolingHttpClientConnectionManager poolConnManager = null;
+	private int maxTotalPool = 200;
+	private int maxConPerRoute = 20;
+	private int socketTimeout = 10000;
+	private int connectionRequestTimeout = 2000;
+	private int connectTimeout = 10000;
+	public static String proxyIp = null;// "10.180.162.66";
+	public static int proxyPort = 1234;
+	private static HttpClientHelper httpClientHelper = null;
+
+	private HttpClientHelper() {
+		init();
+	}
+
+	private static synchronized void syncInit() {
+		if (httpClientHelper == null) {
+			httpClientHelper = new HttpClientHelper();
+		}
+	}
+
+	public static HttpClientHelper getInstance() {
+		if (httpClientHelper == null) {
+			syncInit();
+		}
+		return httpClientHelper;
+	}
+
+	public PoolingHttpClientConnectionManager getConnManager() {
+		PoolingHttpClientConnectionManager cm = null;
+		try {
+			SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+			HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, hostnameVerifier);
+			Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+					.register("http", PlainConnectionSocketFactory.getSocketFactory()).register("https", sslsf).build();
+			cm = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+			cm.setMaxTotal(maxTotalPool);
+			cm.setDefaultMaxPerRoute(maxConPerRoute);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cm;
+	}
+
+	public void init() {
+		try {
+			SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+			HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, hostnameVerifier);
+			Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+					.register("http", PlainConnectionSocketFactory.getSocketFactory()).register("https", sslsf).build();
+			poolConnManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+			// Increase max total connection to 200
+			poolConnManager.setMaxTotal(maxTotalPool);
+			// Increase default max connection per route to 20
+			poolConnManager.setDefaultMaxPerRoute(maxConPerRoute);
+			SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(socketTimeout).build();
+			poolConnManager.setDefaultSocketConfig(socketConfig);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public CloseableHttpClient getConnection() {
+		RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(connectionRequestTimeout)
+				.setConnectTimeout(connectTimeout).setSocketTimeout(socketTimeout).build();
+		HttpClientBuilder httpClientBuilder = HttpClients.custom();
+		httpClientBuilder.setConnectionManager(poolConnManager).setDefaultRequestConfig(requestConfig);// set proxy
+//		if (proxyIp != null) {
+//			org.apache.http.HttpHost proxy = new org.apache.http.HttpHost(proxyIp, Integer.valueOf(proxyPort));
+//			DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+//			httpClientBuilder.setRoutePlanner(routePlanner);
+//		}
+		CloseableHttpClient httpClient = httpClientBuilder.build();
+		if (poolConnManager != null && poolConnManager.getTotalStats() != null) {
+			System.out.println("now client pool " + poolConnManager.getTotalStats().toString());
+		}
+		return httpClient;
+	}
+
+	/**
+	 * ���� GET ����HTTP����������������
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public String doGet(String url) {
+		return doGet(url, new HashMap<>());
+	}
+
+	/**
+	 * ���� GET ����HTTP����K-V��ʽ
+	 * 
+	 * @param url
+	 * @param params
+	 * @return
+	 */
+	public String doGet(String url, Map<String, Object> params) {
+		String apiUrl = url;
+		StringBuffer param = new StringBuffer();
+		int i = 0;
+		for (String key : params.keySet()) {
+			if (i == 0) {
+				param.append("?");
+			} else {
+				param.append("&");
+			}
+			param.append(key).append("=").append(params.get(key));
+			i++;
+		}
+		apiUrl += param;
+		String result = null;
+		CloseableHttpClient httpClient = getConnection();
+		CloseableHttpResponse response = null;
+		HttpGet httpPost = null;
+		try {
+			httpPost = new HttpGet(apiUrl);
+			response = httpClient.execute(httpPost);
+			int status = response.getStatusLine().getStatusCode();
+			if (status >= 200 && status < 300) {
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+					result = EntityUtils.toString(response.getEntity(), "UTF-8");
+				}
+			}
+			EntityUtils.consume(response.getEntity());
+			response.close();
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			httpPost.abort();
+			httpPost.releaseConnection();
+			if (response != null) {
+				try {
+					EntityUtils.consume(response.getEntity());
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
+	public String doPost(String url) {
+		return doPost(url, new HashMap<>(), false);
+	}
+
+	/**
+	 * ���� POST ����HTTP����K-V��ʽ
+	 * 
+	 * @param url    �ӿ�URL
+	 * @param params ����map
+	 * @return
+	 */
+	public String doPost(String url, Map<String, Object> params, boolean isJson) {
+		String result = null;
+		HttpPost httpPost = new HttpPost(url);
+		CloseableHttpClient httpClient = getConnection();
+		CloseableHttpResponse response = null;
+		try {
+			if (isJson) {
+				httpPost.setEntity(new StringEntity(JsonFactory.toJson(params), Charset.forName("UTF-8")));
+			} else {
+				List<NameValuePair> pairList = new ArrayList<NameValuePair>(params.size());
+				for (Map.Entry<String, Object> entry : params.entrySet()) {
+					NameValuePair pair = new BasicNameValuePair(entry.getKey(), entry.getValue().toString());
+					pairList.add(pair);
+				}
+				httpPost.setEntity(new UrlEncodedFormEntity(pairList, Charset.forName("UTF-8")));
+			}
+
+			response = httpClient.execute(httpPost);
+			int status = response.getStatusLine().getStatusCode();
+			if (status >= 200 && status < 300) {
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+					result = EntityUtils.toString(response.getEntity(), "UTF-8");
+				}
+			}
+			EntityUtils.consume(response.getEntity());
+			response.close();
+			System.out.println("Post: " + result);
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			httpPost.releaseConnection();
+			if (response != null) {
+				try {
+					EntityUtils.consume(response.getEntity());
+					response.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+}
